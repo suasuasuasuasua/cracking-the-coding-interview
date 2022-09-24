0bf9752c78339977857283b628e890312aaa4ef0
Justin Hoang <justinhoang@mines.edu>
2022-09-24 14:04:43 -0600

    Created linked list from scratch

---
+++ implementations/linked_list.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node_t
+{
+    int value;
+    struct node_t* next;
+} node;
+
+typedef struct linked_list_t
+{
+    struct node_t* head;
+    struct node_t* tail;
+    int size;
+} linked_list;
+
+/* Given a value, search for the corresponding node in the linked list
+ * */
+node* searchNode(linked_list* llist, int val)
+{
+    node* currentNode = llist->head;
+    node* target;
+    while (currentNode != NULL)
+    {
+        if (currentNode->value == val)
+        {
+            target = currentNode;
+            break;
+        }
+        currentNode = currentNode->next;
+    }
+    return target;
+}
+
+/* Create a new node with the value passed in the parameter 
+ * */
+void insertNode(linked_list* llist, int val)
+{
+    llist->size++;
+    node *newNode;
+    newNode = (node *) malloc(sizeof(node));
+    newNode->value = val;
+    newNode->next = NULL;
+    /* V
+     * 1
+     * ^
+     * If the linked list is empty, then set the head and tail
+     * to the new node 
+     * */
+    if (llist->head == NULL && llist->tail == NULL)
+    {
+        llist->head = newNode;
+        llist->tail = newNode;
+        return;
+    }
+    /* v     V         v
+     * 1     1 > 2     1 > 2 > 3
+     * ^     ^                 ^
+     * Otherwise, add the new pointer to the end of the list
+     * (as the tail pointer's next), then set the new pointer as the tail 
+     * */
+    llist->tail->next = newNode;
+    llist->tail = newNode;
+}
+
+/* Find the first node with the given value and delete it from the list 
+ * */
+void deleteNode(linked_list* llist, int val)
+{
+    /* If the value is the head node, then delete the head node 
+     * and reset the head
+     * Let val = 1
+     * v            v
+     * 1 > 2 > 3    2 > 3
+     *         ^        ^
+     */
+    if (llist->head != NULL && llist->head->value == val)
+    {
+        node* deleted = llist->head;
+        llist->head = llist->head->next;
+        free(deleted);
+        llist->size--;
+        return;
+    }
+
+    node* prevNode = llist->head;
+    node* currentNode;
+    node* nextNode;
+
+    while (prevNode != NULL)
+    {
+        if (prevNode->next != NULL)
+        {
+            currentNode = prevNode->next;
+            nextNode = prevNode->next->next;
+            if (currentNode->value == val)
+            {
+                /* If the value is at the end, delete the node and reset the tail
+                 * v            v    
+                 * 1 > 2 > 3    1 > 2
+                 * ^       ^        ^ */
+                if (nextNode == NULL) llist->tail  = prevNode;
+                /* Else, If the value is somewhere in the middle of the list,
+                 * use the previous node to jump OVER to the next node
+                 * v            v           v
+                 * 1 > 2 > 3    1 > 2 > 3   1 > 3
+                 *         ^    \______/^       ^ */
+                prevNode->next = nextNode;
+                llist->size--;
+                free(currentNode);
+                return;
+            }
+        }
+        prevNode = prevNode->next; // Move to the next node
+    }
+
+}
+
+void printList(linked_list* llist)
+{
+    node* currentNode = llist->head;
+    /* Print each node in the linked list, starting at the head of the list 
+     * */
+    while (currentNode != NULL) {
+        printf("%d ", currentNode->value);
+        currentNode = currentNode->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    linked_list *llist;
+    llist = (linked_list*) malloc(sizeof(linked_list));
+
+    for (int i = 1; i <= 20; i++)
+    {
+        insertNode(llist, i);
+    }
+    for (int i = 20; i >= 1; i--)
+    {
+        printList(llist);
+        deleteNode(llist, i);
+    }
+}
